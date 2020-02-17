81006852fe8aea2e293a24b0495b557ea5bb45c9
Raphael Molina <manson1307@gmail.com>
2020-02-17 12:53:02 -0300

    Retirado uma linha de código dentro da função proximaRodada

--- genius_arduino.ino
+++ genius_arduino.ino
@@ -46,7 +46,6 @@ void loop() {
 /************************* Funções **************************/
 
 void proximaRodada() {
-  RandomSeed(analogRead(A0));
   int sorteio = random(4);
   sequencia[rodada] = sorteio;
   rodada = rodada + 1;
