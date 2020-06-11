bc93aaefbd8b80d275070d027997ba4cddeb4ca1
Wellington Hayner <whayners@hotmail.com>
2020-06-11 15:19:02 -0400

    Fiz um pequeno comentário na função Próxima Rodada
    Apenas para fins didáticos

--- genius_arduino.ino
+++ genius_arduino.ino
@@ -46,6 +46,7 @@ void loop() {
 /************************* Funções **************************/
 
 void proximaRodada() {
+   //Teste Pull Request - Aluno Projeto José de Assis
   randomSeed(analogRead(A0));
   int sorteio = random(4);
   sequencia[rodada] = sorteio;
