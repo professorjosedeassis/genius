db79c28914311c413b0058452eb3c8c99c1546f0
Augusto César <negocios.augusto@gmail.com>
2020-12-22 16:02:02 -0300

    modificado o arquivo genius_arduino.ino

--- genius_arduino.ino
+++ genius_arduino.ino
@@ -46,6 +46,7 @@ void loop() {
 /************************* Funções **************************/
 
 void proximaRodada() {
+  funcaoDeAprendizagemDoCursoDeGitEGitHub();
   randomSeed(analogRead(A0));
   int sorteio = random(4);
   sequencia[rodada] = sorteio;
